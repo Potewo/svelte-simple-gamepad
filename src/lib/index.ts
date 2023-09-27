d7641a9e00ab60d532045dc1333bf100898ab560
Potewo <32093924+Potewo@users.noreply.github.com>
2023-09-28 02:09:29 +0900

    remove exporting types

--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-// Reexport your entry components here
-import Gamepad from './Gamepad.svelte';
-import { layout } from './layout.js';
-import type { GamepadState, State, Layout } from './types.js';
-
-export { Gamepad, layout, GamepadState, State, Layout };
