6b1439be298c84e0c3effac80d29591961e1a948
Potewo <32093924+Potewo@users.noreply.github.com>
2023-09-28 02:03:47 +0900

    export components

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+// Reexport your entry components here
+import Gamepad from './Gamepad.svelte';
+import { layout } from './layout.js';
+import type { GamepadState, State, Layout } from './types.js';
+
+export { Gamepad, layout, GamepadState, State, Layout };
